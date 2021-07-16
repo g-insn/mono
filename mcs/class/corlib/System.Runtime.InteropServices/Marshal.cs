7e1e938c912b68c335b650f2807b72fd02475ad5
Gabriel Ivăncescu <gabrielopcode@gmail.com>
2021-07-16 19:43:29 +0300

    Support objects marshalled as variants in CCWInvokeInternal.

--- mcs/class/corlib/System.Runtime.InteropServices/Marshal.cs
+++ mcs/class/corlib/System.Runtime.InteropServices/Marshal.cs
@@ -2086,8 +2086,9 @@ namespace System.Runtime.InteropServices
 		[MethodImplAttribute (MethodImplOptions.InternalCall)]
 		private extern static bool IsTypeMarshalledAsInterfaceInternal(Type t);
 
-		unsafe private static object ConvertInputArgument(ParameterInfo parm, Variant* varg)
+		unsafe private static object ConvertInputArgument(ParameterInfo parm, Variant* varg, out bool asVariant)
 		{
+			asVariant = false;
 			short vt = varg->vt;
 			object arg;
 
@@ -2122,7 +2123,8 @@ namespace System.Runtime.InteropServices
 
 				if (marshalAs == UnmanagedType.Struct)
 				{
-					throw new ArgumentException();  // FIXME
+					asVariant = true;
+					return GetObjectForNativeVariant((IntPtr)varg);
 				}
 				if (t.IsArray || marshalAs == UnmanagedType.SafeArray || marshalAs == UnmanagedType.LPArray)
 				{
@@ -2166,7 +2168,7 @@ namespace System.Runtime.InteropServices
 			return arg;
 		}
 
-		unsafe private static void ConvertOutputArgument(ParameterInfo parm, Variant* varg, object arg)
+		unsafe private static void ConvertOutputArgument(ParameterInfo parm, Variant* varg, object arg, bool asVariant)
 		{
 			Variant v = default (Variant);
 			short vt = varg->vt;
@@ -2174,6 +2176,14 @@ namespace System.Runtime.InteropServices
 			if (vt == ((short)VarEnum.VT_VARIANT | (short)VarEnum.VT_BYREF))
 			{
 				varg = (Variant*)varg->pdispVal;
+				if (asVariant)
+				{
+					v = *varg;
+					v.Clear();
+					v.SetValue(arg);
+					*varg = v;
+					return;
+				}
 				vt = varg->vt;
 			}
 			bool vtbyref = (vt & (short)VarEnum.VT_BYREF) != 0;
@@ -2257,13 +2267,14 @@ namespace System.Runtime.InteropServices
 		{
 			ParameterInfo[] parms = m.GetParameters();
 			object[] args = new object[parms.Length];
+			bool[] asVariant = new bool[parms.Length];
 
 			try
 			{
 				foreach (ParameterInfo parm in parms)
 				{
 					int argpos = parm.Position;
-					args[argpos] = ConvertInputArgument(parm, argmap[argpos]);
+					args[argpos] = ConvertInputArgument(parm, argmap[argpos], out asVariant[argpos]);
 				}
 
 				object res = m.Invoke(obj, args);
@@ -2282,7 +2293,7 @@ namespace System.Runtime.InteropServices
 						continue;
 					try
 					{
-						ConvertOutputArgument(parm, varg, args[argpos]);
+						ConvertOutputArgument(parm, varg, args[argpos], asVariant[argpos]);
 					}
 					catch
 					{
